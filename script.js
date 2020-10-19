e6b3d40e076ef428ac0e9c95f974571eab103a29
Amey <ameykh@cybage.com>
2020-10-19 14:35:34 +0530

    Inital Commit

---
+++ script.js
@@ -0,0 +1,64 @@
+$(document).ready(() => {
+  const btnId = ["btn1", "btn2", "btn3"];
+  const color = ["red", "yellow", "orange"];
+  $(".btn").each(function (i) {
+    $(this).click(function () {
+      $(".ball").each(function () {
+        if (!$(this).hasClass(color[i])) {
+          $(this).hide();
+        }
+      });
+      $(".green").removeClass("green");
+      $(this).addClass("green");
+    });
+  });
+
+  $("#btn4").click(function () {
+    $(".ball").each(function () {
+      $(this).show();
+    });
+    $(".green").removeClass("green");
+    $(this).addClass("green");
+  });
+});
+
+//--------Selecting each button and then adding event listner------------
+// $("#btn1").click(() => {
+//   $(".ball").each(function () {
+//     if (!$(this).hasClass("red")) {
+//       $(this).hide();
+//     }
+//   });
+//   $(".btn").addClass(function () {
+//     if (this.id === "btn1") {
+//       return "green";
+//     }
+//     this.classList.remove("green");
+//   });
+// });
+// $("#btn2").click(() => {
+//   $(".ball").each(function () {
+//     if (!$(this).hasClass("yellow")) {
+//       $(this).hide();
+//     }
+//   });
+//   $(".btn").addClass(function () {
+//     if (this.id === "btn2") {
+//       return "green";
+//     }
+//     this.classList.remove("green");
+//   });
+// });
+// $("#btn3").click(() => {
+//   $(".ball").each(function () {
+//     if (!$(this).hasClass("orange")) {
+//       $(this).hide();
+//     }
+//   });
+//   $(".btn").addClass(function () {
+//     if (this.id === "btn3") {
+//       return "green";
+//     }
+//     this.classList.remove("green");
+//   });
+// });
