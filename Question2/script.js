9a6268fd6a140796707fbc7ecf78a1a6bfd0c4e1
Amey <ameykh@cybage.com>
2020-10-19 15:14:47 +0530

    Second Question

---
+++ Question2/script.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/users",
+    method: "GET",
+    success: (resp) => {
+      resp.forEach((ele) => {
+        let res = "<tbody><tr>";
+        res += `<td>${ele.id}</td>`;
+        res += `<td>${ele.name}</td>`;
+        res += `<td>${ele.username}</td>`;
+        res += `<td>${ele.email}</td>`;
+        res += `<td>${ele.phone}</td>`;
+        res += `<td>${ele.website}</td>`;
+        res += "</tr></tbody>";
+        console.log(res);
+        $("#mytable").append(res);
+      });
+    },
+    error: (err) => {
+      console.log(err);
+    },
+  });
+
+  $("#mybtn").click(() => {
+    $("#mytable").DataTable();
+  });
+});
